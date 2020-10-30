5692fc8c3aac7fba52bc1dcd991b31592c415583
ywr <ophthalmencephalon@gmail.com>
2020-10-30 18:36:30 +0000

    add discord oauth basics

--- discoauth/__init__.py
+++ discoauth/__init__.py
@@ -1,5 +1,14 @@
-from flask import flask
+import os
+from flask import Flask
+
+
+from .oauth.discord_oauth import discord_oauth
+
+
+
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+app.secret_key = bytes(os.getenv('SB_FLASK_SECRET_KEY'), 'utf-8').decode('unicode_escape')
+app.register_blueprint(discord_oauth, url_prefix='/auth')
---
+++ discoauth/oauth/discord_oauth.py
@@ -0,0 +1,116 @@
+from flask import Blueprint, request, session, redirect, url_for
+from requests_oauthlib import OAuth2Session
+from hashlib import sha256
+import os
+
+DISCORD_OAUTH2_CLIENT_ID = os.getenv('SB_DISCORD_OAUTH2_CLIENT_ID')
+DISCORD_OAUTH2_CLIENT_SECRET = os.getenv('SB_DISCORD_OAUTH2_CLIENT_SECRET')
+DISCORD_OAUTH2_REDIRECT_URI = os.getenv('SB_DISCORD_OAUTH2_REDIRECT_URI')
+
+DISCORD_API_BASE_URL = os.getenv('DISCORD_API_BASE_URL', 'https://discordapp.com/api')
+DISCORD_AUTHORIZATION_BASE_URL = DISCORD_API_BASE_URL + '/oauth2/authorize'
+DISCORD_TOKEN_URL = DISCORD_API_BASE_URL + '/oauth2/token'
+
+discord_oauth = Blueprint('discord_oauth', __name__)
+
+if 'http://' in DISCORD_OAUTH2_REDIRECT_URI:
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
+
+def discord_token_updater(token):
+    discord_session['oauth2_token'] = token
+
+def discord_guild_token_updater(token):
+    discord_guild_session['oauth2_token'] = token
+
+def make_discord_session(token=None, state=None, scope=None):
+    return OAuth2Session(
+        client_id=DISCORD_OAUTH2_CLIENT_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=DISCORD_OAUTH2_REDIRECT_URI,
+        auto_refresh_kwargs={
+            'client_id': DISCORD_OAUTH2_CLIENT_ID,
+            'client_secret': DISCORD_OAUTH2_CLIENT_SECRET,
+        },
+        auto_refresh_url=DISCORD_TOKEN_URL,
+        token_updater=discord_token_updater)
+
+@discord_oauth.route('/discord')
+def authorize_discord():
+    join = request.args.get('guid', default = None, type = str)
+    link = request.args.get('link', default = None, type = str)
+    if join:
+        join = sha256(join.encode('utf-8')).hexdigest()
+        join_param = f'_{join}'
+        scope = request.args.get(
+            'scope',
+            'identify connections guilds.join'
+        )
+        discord = make_discord_session(scope=scope.split(' '))
+        authorization_url, state = discord.authorization_url(DISCORD_AUTHORIZATION_BASE_URL)
+#This is ugly, but I'm using it to avoid registering additional callback URIs with discord
+#Maybe I should be using new_state()
+#I definitely shouldn't be repeating this ugly pattern like I am
+        state_param = state
+        if link:
+            link = sha256(link.encode('utf-8')).hexdigest()
+            link_param = f'-{link}'
+            discord = make_discord_session(scope=scope.split(' '), state=f'{state_param}_{join}-{link}')
+        else:
+            discord = make_discord_session(scope=scope.split(' '), state=f'{state_param}_{join}')
+        authorization_url, state = discord.authorization_url(DISCORD_AUTHORIZATION_BASE_URL)
+    else:
+        scope = request.args.get(
+            'scope',
+            'identify connections'
+        )
+        discord = make_discord_session(scope=scope.split(' '))
+        authorization_url, state = discord.authorization_url(DISCORD_AUTHORIZATION_BASE_URL)
+        state_param = state
+        if link:
+            link = sha256(link.encode('utf-8')).hexdigest()
+            link_param = f'-{link}'
+            discord = make_discord_session(scope=scope.split(' '), state=f'{state_param}-{link}')
+        else:
+            discord = make_discord_session(scope=scope.split(' '), state=f'{state_param}')
+        authorization_url, state = discord.authorization_url(DISCORD_AUTHORIZATION_BASE_URL)
+    print(f'state is {state}')
+    session['oauth2_state'] = state
+    print(f"state is {state}, session is {session['oauth2_state']}")
+    return redirect(authorization_url)
+
+@discord_oauth.route('/callback_discord')
+def callback_discord():
+    rstate = request.args.get('state', default = None, type = str)
+#this "link" value is to support hooking to additional third party services after authentication
+    if "-" in rstate:
+        link = rstate.split('-')[-1]
+        rstate = rstate.split('-', 1)[0]
+        if "_" in rstate:
+            guid = rstate.split('_')[-1]
+        else:
+            guid = None
+    else:
+        link = None
+    if "_" in rstate:
+        guid = rstate.split('_')[-1]
+    else:
+        guid = None
+    if request.values.get('error'):
+        return request.values['error']
+    discord = make_discord_session(state=session.get('oauth2_state'))
+    token = discord.fetch_token(
+        DISCORD_TOKEN_URL,
+        client_secret=DISCORD_OAUTH2_CLIENT_SECRET,
+        authorization_response=request.url)
+    session['oauth2_token'] = token
+#    i have a token now and i should be storing it
+    return redirect(url_for('.confirmation', service=sha256('Discord'.encode('utf-8')).hexdigest(), guid=guid, link=link))
+
+@discord_oauth.route('/confirmation')
+def confirmation():
+    service = request.args.get('service', default=None, type=str)
+    guid = request.args.get('guid', default=None, type=str)
+    link = request.args.get('link', default=None, type=str)
+    return f"ayyyy lmao your service is {service}, your link is {link} and your guid is {guid}"
