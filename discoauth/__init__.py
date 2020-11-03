7905194a7ed7a9c5dd7c83ce2c6d3e8cfa1899a1
ywr <ophthalmencephalon@gmail.com>
2020-11-03 08:42:49 +0000

    make things worse and commit them anyway

--- discoauth/__init__.py
+++ discoauth/__init__.py
@@ -11,7 +11,7 @@ from .models import AffiliatedGuild, SupportedService
 from .oauth import discord_oauth
 
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask('discoauth', instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 app.secret_key = bytes(os.getenv('SB_FLASK_SECRET_KEY'), 'utf-8').decode('unicode_escape')
@@ -20,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SB_DATABASE_URL']
 app.register_blueprint(discord_oauth, url_prefix='/auth')
 db.init_app(app)
 
-
+migrate = Migrate(app, db)
 
 @app.before_first_request
 def populate_static_tables():
