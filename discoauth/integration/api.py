573a47cabd730fd63109d6a0e18205ca414f1e32
amazingproducer <amazingproducer@gmail.com>
2021-02-13 10:36:33 +0000

    Add basic authorization for github search queries

--- discoauth/integration/api.py
+++ discoauth/integration/api.py
@@ -10,12 +10,22 @@ from discoauth import create_app, db
 from discoauth.models import User, UserOAuth, AffiliatedGuild, ServiceVerification, SupportedService 
 
 bp_discoauth_api = Blueprint('bp_discoauth_api', __name__)
+authorized_apps = []
+authorized_apps.append(os.getenv('SB_PROMOBOT_AUTH_KEY'))
+authorized_apps.append(os.getenv('SB_ARGENT_AUTH_KEY'))
 
 app = create_app()
 app.app_context().push()
 
 @bp_discoauth_api.route('/users/github/<snowflake>')
 def get_user(snowflake):
+    authorized_request = False
+    if "Authorization" in request.headers:
+        if request.headers["Authorization"] in authorized_apps:
+            authorized_request = True
+    if not authorized_request:
+        return jsonify({"error":"unauthorized"}), 403
+
 #    known_user = UserOauth.query.filter_by(discord_user_id=str(snowflake)).first()
     known_user = UserOAuth.query.filter_by(
         discord_user_id=str(snowflake),
